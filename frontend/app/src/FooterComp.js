4fb9c0d969624ff21f26a3b0205fcafd4a99e5ea
Aryan Deshpande <deshpandearyan1@gmail.com>
2023-03-09 22:22:48 +0530

    (Frontend)[Made Major Changes, Added animation and formating]

---
+++ frontend/app/src/FooterComp.js
@@ -0,0 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import React from 'react';
+
+function FooterComponent(){
+    return(
+        <Navbar bg="dark" variant="dark" className="justified-content-center" >
+        <Container>
+          <Navbar.Brand href="#aryan">Aryan Deshpande</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="#resume">Something is cooking</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    )
+}
+
+export default FooterComponent
--- frontend/app/src/NavbarComp.js
+++ frontend/app/src/NavbarComp.js
@@ -7,13 +7,14 @@ import React from 'react';
 
 function NavbarComp(){
     return(
-        <Navbar bg="light" variant="light">
+        <Navbar bg="light" variant="light" className="justified-content-center" fixed='top'>
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Navbar.Brand href="#aryan">Aryan Deshpande</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="#projects" onClick={() => window.location.replace("/#projects")}>Projects</Nav.Link>
+            <Nav.Link href="#blogs" onClick={() => window.location.replace("/#blogs")}>Blogs</Nav.Link>
+            <Nav.Link href="#stack" onClick={() => window.location.replace("/#stack")}>Stack</Nav.Link>
+            <Nav.Link href="https://cdn.discordapp.com/attachments/835750351621718030/1083423477979164772/Text_to_Image_Synthesis_for_Improved_Image_Captioning.pdf">Resume</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
